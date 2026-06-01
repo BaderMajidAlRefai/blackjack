f588453324925f34bbb68b926795be3eee56a505
Bader Al Refai <badermajidalrefai@gmail.com>
2026-06-02 02:14:08 +0300

    Added handling of no money

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import game_functions
+import time
+import sys
 chips = 100
 print("Welcome to BlackJack!")
 
 
 while True:
+    if chips == 0:
+        print("awwweee you have no money...")
+        time.sleep(2)
+        print("get out.")
+        time.sleep(0.5)
+        sys.exit
+
     print(f"You currently have: {chips} chips!")
     input("Please enter any key to start the game.")
     game = game_functions.game(chips)
